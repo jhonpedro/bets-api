26ef52559de6e3a1731592e9282dfe7978e80460
João Pedro <pedrobarros2010@gmail.com>
2021-06-18 16:28:11 -0300

    feat: add relations and afterCreate hook

--- app/Models/Purchase.js
+++ app/Models/Purchase.js
@@ -4,6 +4,19 @@
 const Model = use('Model')
 
 class Purchase extends Model {
+  static boot() {
+    super.boot()
+
+    this.addHook('afterCreate', 'NewPurchaseHook.NewPurchaseMail')
+  }
+
+  user() {
+    return this.belongsTo('App/Models/User')
+  }
+
+  bets() {
+    return this.hasMany('App/Models/Bet')
+  }
 }
 
 module.exports = Purchase
