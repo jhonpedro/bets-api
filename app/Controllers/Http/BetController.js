f4cf455b5e52830d196e73f6a6b5dcbc99bc0339
João Pedro <pedrobarros2010@gmail.com>
2021-06-21 11:01:39 -0300

    feat: create bets controller

---
+++ app/Controllers/Http/BetController.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const formatNumbersCSVToNumbersArr = require('../../../util/formatNumbersCSVToNumbersArr')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
+const Database = use('Database')
+
+/**
+ * Resourceful controller for interacting with bets
+ */
+class BetController {
+  /**
+   * Show a list of all bets.
+   * GET bets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async index({ auth }) {
+    const bets = await Database.table('bets')
+      .innerJoin('purchases', 'bets.purchase_id', 'purchases.id')
+      .where('purchases.user_id', auth.user.id)
+      .orderBy('bets.created_at', 'desc')
+
+    return bets.map((bet) => ({
+      ...bet,
+      numbers: formatNumbersCSVToNumbersArr(bet.numbers),
+    }))
+  }
+}
+
+module.exports = BetController
