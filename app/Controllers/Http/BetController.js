fffbefe0efd16db5ebc848616c6b2b8b9d26a80f
João Pedro <pedrobarros2010@gmail.com>
2021-06-22 14:02:30 -0300

    feat: add join games table and select only what web needs

--- app/Controllers/Http/BetController.js
+++ app/Controllers/Http/BetController.js
@@ -21,7 +21,9 @@ class BetController {
    */
   async index({ auth }) {
     const bets = await Database.table('bets')
+      .select('games.type', 'games.color', 'games.price', 'bets.numbers')
       .innerJoin('purchases', 'bets.purchase_id', 'purchases.id')
+      .innerJoin('games', 'bets.game_id', 'games.id')
       .where('purchases.user_id', auth.user.id)
       .orderBy('bets.created_at', 'desc')
 
